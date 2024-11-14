31fc8233eb8ef2b0ac25194505abef777f2557c3
jacobdavidson <jddavid@umich.edu>
2024-11-14 18:56:28 +0100

    Update install_update_beesbook_pipeline.sh
    now installing both pytorch and tensorflow with conda to ensure compatibility, and specifying the python and cuda version

--- code/install_update_beesbook_pipeline.sh
+++ code/install_update_beesbook_pipeline.sh
@@ -1,37 +1,58 @@
 #!/bin/bash
 
-# exit if anything in the script fails
-set -e 
+# Exit if any command fails
+set -e
+
 # Source conda setup to ensure conda commands are available
 eval "$(conda shell.bash hook)"
 
-# for creating the conda environment
+# Configuration
 ENV_NAME="beesbook"
+PYTHON_VERSION="3.12"
+CUDA_VERSION="12.4.1"  #  should match the versions and be available on https://anaconda.org/nvidia/cuda-toolkit and https://anaconda.org/nvidia/cuda
+CUDA_VERSION_SHORT="${CUDA_VERSION%.*}"  # Short format, e.g., 12.4, for PyTorch compatibility
+
 
 # Check if the Conda environment exists
 if conda env list | grep -q "$ENV_NAME"; then
     echo "Conda environment '$ENV_NAME' already exists. Activating..."
 else
-    echo "Creating Conda environment '$ENV_NAME'..."
-    conda create -n "$ENV_NAME" python=3.11 -y
+    echo "Creating Conda environment '$ENV_NAME' with Python $PYTHON_VERSION..."
+    conda create -n "$ENV_NAME" python="$PYTHON_VERSION" -y
 fi
 
 conda activate "$ENV_NAME"
 
-# Install core Python packages if not already installed
-echo "Installing core Python packages..."
-conda install --yes python=3.11 
-conda install --yes jupyterlab matplotlib scipy seaborn jupyter numpy
-conda install --yes ffmpeg dill tqdm chardet
-conda install --yes -c conda-forge cairocffi
+# Ensure pip is correctly installed in this Conda environment
+conda install -n "$ENV_NAME" pip -y
 
+# Install core Python packages
+echo "Installing core Python packages..."
+conda install --yes python="$PYTHON_VERSION"  # in case the environment already exists
+conda install --yes jupyterlab matplotlib scipy seaborn jupyter numpy ffmpeg dill tqdm chardet
+python -m pip install cairocffi  # install with pip because it's faster
+conda install --yes -c conda-forge lapack blas
+
+
+# Install TensorFlow
+echo "Installing tensorflow"
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    pip install --upgrade tensorflow
+    conda install -y pytorch::pytorch torchvision torchaudio -c pytorch
+    conda install -y conda-forge::tensorflow
 else
-    pip install --upgrade tensorflow[and-cuda]
+    # Install CUDA toolkit and cuDNN via conda
+    echo "Installing CUDA Toolkit $CUDA_VERSION and compatible cuDNN version via conda..."
+    conda install -y nvidia/label/cuda-"$CUDA_VERSION"::cuda
+    conda install -y nvidia/label/cuda-"$CUDA_VERSION"::cuda-toolkit
+    conda install -y nvidia::cudnn
+
+    # Install PyTorch with specified CUDA version
+    echo "Installing PyTorch with CUDA $CUDA_VERSION_SHORT..."
+    conda install -y pytorch torchvision torchaudio pytorch-cuda="$CUDA_VERSION_SHORT" -c pytorch -c nvidia
+    conda install -y conda-forge::tensorflow-gpu
 fi
 
-# List of GitHub repositories and their package names, separated by a space
+# List of GitHub repositories and their package names
 REPOS=(
     "git+https://github.com/BioroboticsLab/bb_binary bb_binary"
     "git+https://github.com/BioroboticsLab/bb_pipeline bb_pipeline"
@@ -46,7 +67,7 @@ for repo in "${REPOS[@]}"; do
     repo_url=$(echo "$repo" | awk '{print $1}')
     package_name=$(echo "$repo" | awk '{print $2}')
     # Uninstall the package
-    python -m pip uninstall -y "$package_name"  
+    python -m pip uninstall -y "$package_name"
     # Install or upgrade the package using pip
     python -m pip install --upgrade "$repo_url"
 done
@@ -55,26 +76,16 @@ done
 BB_PIPELINE_DIR=$(python -c "import pipeline; print('pipeline_path:'); print(pipeline.__path__[0])" | awk '/^pipeline_path:/{getline; print}')
 CONFIG_FILE="$BB_PIPELINE_DIR/config.ini"
 
-# Create a directory for model files in the conda environment
+# Re-download the model files and overwrite if they exist
 MODEL_DIR="$CONDA_PREFIX/pipeline_models"
 mkdir -p $MODEL_DIR
 
-# Download the model files if they don't already exist.  Also download the tracklet model file
-if [ ! -f "$MODEL_DIR/decoder_2019_keras3.h5" ]; then
-    wget -O $MODEL_DIR/decoder_2019_keras3.h5 "https://github.com/BioroboticsLab/bb_pipeline_models/blob/master/models/decoder/decoder_2019_keras3.h5?raw=true"
-fi
-if [ ! -f "$MODEL_DIR/localizer_2019_keras3.h5" ]; then
-    wget -O $MODEL_DIR/localizer_2019_keras3.h5 "https://github.com/BioroboticsLab/bb_pipeline_models/blob/master/models/saliency/localizer_2019_keras3.h5?raw=true"
-fi
-if [ ! -f "$MODEL_DIR/localizer_2019_attributes.json" ]; then
-    wget -O $MODEL_DIR/localizer_2019_attributes.json "https://github.com/BioroboticsLab/bb_pipeline_models/blob/master/models/saliency/localizer_2019_attributes.json?raw=true"
-fi
-if [ ! -f "$MODEL_DIR/detection_model_4.json" ]; then
-    wget -O $MODEL_DIR/detection_model_4.json "https://github.com/BioroboticsLab/bb_pipeline_models/blob/master/models/tracking/detection_model_4.json?raw=true"
-fi
-if [ ! -f "$MODEL_DIR/tracklet_model_8.json" ]; then
-    wget -O $MODEL_DIR/tracklet_model_8.json "https://github.com/BioroboticsLab/bb_pipeline_models/blob/master/models/tracking/tracklet_model_8.json?raw=true"
-fi
+# Download and replace model files
+wget -O $MODEL_DIR/decoder_2019_keras3.h5 "https://github.com/BioroboticsLab/bb_pipeline_models/blob/master/models/decoder/decoder_2019_keras3.h5?raw=true"
+wget -O $MODEL_DIR/localizer_2019_keras3.h5 "https://github.com/BioroboticsLab/bb_pipeline_models/blob/master/models/saliency/localizer_2019_keras3.h5?raw=true"
+wget -O $MODEL_DIR/localizer_2019_attributes.json "https://github.com/BioroboticsLab/bb_pipeline_models/blob/master/models/saliency/localizer_2019_attributes.json?raw=true"
+wget -O $MODEL_DIR/detection_model_4.json "https://github.com/BioroboticsLab/bb_pipeline_models/blob/master/models/tracking/detection_model_4.json?raw=true"
+wget -O $MODEL_DIR/tracklet_model_8.json "https://github.com/BioroboticsLab/bb_pipeline_models/blob/master/models/tracking/tracklet_model_8.json?raw=true"
 
 # Update pipeline/config.ini to point to local model files
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -88,26 +99,5 @@ else
     sed -i "s|model_path=localizer_2019_keras3.h5|model_path=$MODEL_DIR/localizer_2019_keras3.h5|g" $CONFIG_FILE
     sed -i "s|attributes_path=localizer_2019_attributes.json|attributes_path=$MODEL_DIR/localizer_2019_attributes.json|g" $CONFIG_FILE
 fi
-
-# Install cuDNN if not already installed
-if [ ! -f "$CONDA_PREFIX/lib/libcudnn.so" ]; then
-    # Download the cuDNN package
-    CUDNN_PKG="cudnn-linux-x86_64-8.9.6.50_cuda11-archive"
-    wget "https://developer.download.nvidia.com/compute/cudnn/redist/cudnn/linux-x86_64/$CUDNN_PKG.tar.xz"
-    tar -xvf "$CUDNN_PKG.tar.xz"
-
-    # Move the extracted files to the conda environment
-    cp "$CUDNN_PKG/include/"* $CONDA_PREFIX/include/.
-    cp "$CUDNN_PKG/lib/"* $CONDA_PREFIX/lib/.
-
-    # Cleanup
-    rm -rf $CUDNN_PKG
-    rm $CUDNN_PKG.tar.xz
-fi
-
-# Ensure the cuDNN libraries are found
-mkdir -p $CONDA_PREFIX/etc/conda/activate.d
-echo 'export PATH=$CONDA_PREFIX/bin:$PATH' > $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-echo 'export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 
 echo "Installation and update completed."
